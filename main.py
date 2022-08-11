2fe3df1fe06d1042c4592a884a68511c85b98cf3
Amanda Watson <aawatson@seas.upenn.edu>
2022-08-11 15:08:35 -0400

    Added Fluid Figure

---
+++ main.py
@@ -0,0 +1,26 @@
+
+#import Algorithm
+#from Figures import IMWUT_figures
+#from Data import aurprocess_medium_exp
+#import Figures.Green_theory_Graphs
+import datetime
+import time
+#import Figures.Air_Medium_Detection_Exp
+#from Data.pull_data import pull_data
+#import Figures.fluid
+import Figures.Intensity_Calibration
+
+if __name__ == '__main__':
+
+    """path = "temp_exp/Cold"
+    print('Pulling Data')
+    tstart = int(time.mktime(datetime.date(2021, 10, 31).timetuple()) * 1000)
+    tend = int(time.mktime(datetime.date(2021, 11, 1).timetuple()) * 1000)
+    pull_data(path,tstart,tend)"""
+
+    print('hi')
+
+
+
+
+
